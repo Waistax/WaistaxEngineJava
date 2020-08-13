b04401de507d5fd2b5eef348d3fd7aa8aa830336
Cem GEÇGEL <aa-albara@hotmail.com>
2020-08-13 17:30:40 +0300

    0.1 Test for Input and Rendering

--- WaistaxEngineJava/src/waistax/sandbox/Sandbox.java
+++ WaistaxEngineJava/src/waistax/sandbox/Sandbox.java
@@ -4,6 +4,7 @@ import waistax.engine.*;
 import waistax.engine.renderer.*;
 
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Tests engine
@@ -25,12 +26,15 @@ public class Sandbox implements Game
 	}
 	
 	private Graphics2D g;
+	private int counter;
+	private int xpos;
 	
 	@Override
 	public void load()
 	{
 		System.out.println("Loading sandbox...");
 		
+		// Get the graphics, then set the color and the font
 		g = ((AWTRenderer) Engine.renderer).graphics;
 		g.setFont(new Font("Verdena", Font.ITALIC, 20));
 		g.setColor(new Color(1.0F, 1.0F, 0.6F, 1.0F));
@@ -45,6 +49,20 @@ public class Sandbox implements Game
 	@Override
 	public void frame()
 	{
-		g.drawString("FPS: " + Engine.frameRate, 10, 30);
+		// Get the input
+		Input i = Engine.renderer.getInput();
+		
+		// Increase the counter if F is pressed
+		if (i.keyPressed[KeyEvent.VK_F]) counter++;
+		
+		// Change the x position of the text with the scroll
+		xpos += i.scroll * 10;
+		
+		// Draw the text
+		g.drawString("FPS: " + Engine.frameRate, xpos, 30);
+		g.drawString("Cursor: " + i.cursor + " Change: " + i.cursorChange, xpos, 50);
+		g.drawString("F is pressed " + counter + " times.", xpos, 70);
+		// If the space bar is down
+		if (i.keyDown[KeyEvent.VK_SPACE]) g.drawString("SPACE IS DOWN!", xpos, 90);
 	}
 }
