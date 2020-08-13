23c46bb1f94c14b37d9c20f2f78d63d00b4bc3c9
Cem GEÇGEL <aa-albara@hotmail.com>
2020-08-13 14:52:33 +0300

    Added Engine class for a test commit

---
+++ WaistaxEngineJava/src/waistax/engine/Engine.java
@@ -0,0 +1,17 @@
+package waistax.engine;
+
+/**
+ * The heart of the engine
+ * 
+ * Has the main loop in it.
+ *
+ * Author: Waistax
+ * Created: 0.1 / 13 Aðu 2020 / 14:46:55
+ *
+ */
+public class Engine
+{
+	/** Private constructor 
+	 * Disables creation of an instance of this class. */
+	private Engine() {}
+}
